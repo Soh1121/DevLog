eb300d45bcf716dc318c253e32c58ae1274fe881
Soh1121 <satou.shg@gmail.com>
2019-08-04 16:01:21 +0900

    [update]更新日時とカテゴリを表示
    get_the_date()とthe_category()を追加

--- index.php
+++ index.php
@@ -41,9 +41,18 @@
   <?php if (have_posts()): while (have_posts()): the_post(); ?>
   <article <?php post_class(); ?>>
   <h2><?php the_title(); ?></h2>
-  <time datetime="<?php echo get_the_date('Y-m-d')?>">
-  <?php echo get_the_date(); ?>
-  </time>
+  <div class="postinfo">
+    <time datetime="<?php echo get_the_date('Y-m-d')?>">
+    <i class="far fa-clock"></i>
+    <?php echo get_the_date(); ?>
+    </time>
+
+    <span class="postcat">
+      <i class="fas fa-folder-open"></i>
+      <?php the_category(','); ?>
+    </span>
+  </div>
+
   <?php the_content(); ?>
   </article>
 <?php endwhile; endif; ?>
