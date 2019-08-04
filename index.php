7c87cda87eb8d7b1f32bf3a9e8132f2f22d0c8f5
Soh1121 <satou.shg@gmail.com>
2019-08-04 14:08:15 +0900

    [update]投稿日を表示
    timeタグを使って日時を表示

--- index.php
+++ index.php
@@ -41,6 +41,9 @@
   <?php if (have_posts()): while (have_posts()): the_post(); ?>
   <article <?php post_class(); ?>>
   <h2><?php the_title(); ?></h2>
+  <time datetime="<?php echo get_the_date('Y-m-d')?>">
+  <?php echo get_the_date(); ?>
+  </time>
   <?php the_content(); ?>
   </article>
 <?php endwhile; endif; ?>
