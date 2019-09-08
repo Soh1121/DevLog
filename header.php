bf0f9ae28228d4de07fe3180b1fc569dad3a5fbe
Soh1121 <satou.shg@gmail.com>
2019-09-08 18:36:50 +0900

    [update]bodyタグにページの種類に応じたクラス名を付与
    動的にできる部分を変更 #48

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <script src="https://kit.fontawesome.com/879fc12ee5.js"></script>
   <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
   <header>
     <div class="container">
       <div id="logo" class="leftcol">
