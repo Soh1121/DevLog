1816c375d28af73d0b5a27aa5bbff1ed442ff9ee
Soh1121 <satou.shg@gmail.com>
2019-09-08 18:30:59 +0900

    [update]ページタイトルを出力
    動的にできる部分を変更 #48

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>DevLog - Sohのエンジニアブログ</title>
+  <title><?php wp_title(' | ', true, 'right'); ?> - <?php bloginfo('description'); ?></title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/destyle.css">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/toppage.css">
