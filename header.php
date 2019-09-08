dc8e21f76717b29d5497ebb55691c53e66629f59
Soh1121 <satou.shg@gmail.com>
2019-09-08 18:17:39 +0900

    [update]toppage.htmlからヘッダー部分をコピー
    動的に変化させる準備のため #48

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>DevLog - Sohのエンジニアブログ</title>
+  <link rel="stylesheet" href="css/destyle.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/toppage.css">
+  <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/879fc12ee5.js"></script>
+
+</head>
+<body>
+  <header>
+    <div class="container">
+      <div id="logo" class="leftcol">
+        <h1>DevLog</h1>
+      </div>
+      <div id="nav">
+        <div id="responsive-btn">
+          <span id="menu-btn">
+            <i class="fas fa-bars"></i>
+          </span>
+          <span id="close-btn">
+            <i class="fas fa-times"></i>
+          </span>
+        </div>
+        <nav id="menu">
+          <ul class="menu">
+            <li><a href="profile.html">PROFILE</a></li>
+            <li><a href="#">BLOG</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
