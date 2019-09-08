696845cdf9706e8a6f7773c2bd3da94a663c2cd9
Soh1121 <satou.shg@gmail.com>
2019-09-08 19:02:39 +0900

    [update]toppage.htmlからフッター部分をコピー
    動的に変化させる準備のため #49

---
+++ footer.php
@@ -0,0 +1,7 @@
+  <footer>
+    <small class="abel uppercase">&copy; Soichi SATO, All Rights Reserved.</small>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="script.js"></script>
+</body>
+</html>
