439a561dd27a1490ec6cb772ed9d78cb333b31b8
Soh1121 <satou.shg@gmail.com>
2019-09-08 19:07:55 +0900

    [update]script.jsのURLを適切に設定
    動的にできる部分を変更 #49

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
     <small class="abel uppercase">&copy; Soichi SATO, All Rights Reserved.</small>
   </footer>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="script.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
 </body>
 </html>
