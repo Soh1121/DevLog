9b85c90c0d4cce1e55ac74285b2353fa9753d622
Soh1121 <satou.shg@gmail.com>
2019-09-08 19:12:24 +0900

    [update]wp_footerを出力
    動的にできる部分を変更 #49

--- footer.php
+++ footer.php
@@ -3,5 +3,6 @@
   </footer>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+  <?php wp_footer(); ?>
 </body>
 </html>
