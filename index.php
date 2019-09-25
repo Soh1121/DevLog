155e7906dc06ad5a5c5d038b7d8e33905071b656
Soh1121 <satou.shg@gmail.com>
2019-09-25 22:21:39 +0900

    [update]記事をarticleで囲む
    個別記事を区別できるように #55

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
-<?php the_title(); ?>
+<article <?php post_class(); ?>>
+<h1><?php the_title(); ?></h1>
 
 <?php the_content(); ?>
+</article>
 
 <?php endwhile; endif; ?>
 
