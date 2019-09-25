b1dd210aa68988eb389dab0d662d9ce23db80661
Soh1121 <satou.shg@gmail.com>
2019-09-25 22:51:06 +0900

    [update]ナビゲーションを付与
    前後の一覧に移動できるようにリンク付けとレイアウトの整理を少々 #55

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
+<div class="container">
+
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
 <article <?php post_class(); ?>>
-<h1><?php the_title(); ?></h1>
+<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
 <div class="postinfo">
   <time datetime="<?php echo get_the_date('Y-m-d') ?>">
@@ -21,6 +23,20 @@
 </article>
 
 <?php endwhile; endif; ?>
+
+<?php if($wp_query->max_num_pages > 1): ?>
+  <div class="pagenav">
+    <span class="navleft">
+      <?php previous_posts_link('<i class="fas fa-chevron-circle-left"></i> 新しい記事'); ?>
+    </span>
+    
+    <span class="navright">
+      <?php next_posts_link('古い記事 <i class="fas fa-chevron-circle-right"></i>'); ?>
+    </span>
+  </div>
+<?php endif;?>
+
+</div>
 
 <?php get_sidebar(); ?>
 
