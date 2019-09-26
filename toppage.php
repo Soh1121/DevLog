a3a6aa07a858a8e14c9deee4027cc5a95aaf7a6a
Soh1121 <satou.shg@gmail.com>
2019-09-27 00:12:20 +0900

    [update]ポートフォリオ部分の実装
    ポートフォリオカテゴリの記事を３つ表示 #56

--- toppage.php
+++ toppage.php
@@ -36,19 +36,35 @@ Template Name: トップページ
         </div>
       </div>
     </section>
+
+    <?php
+    // --- 新着ポートフォリオを3件表示 ---
+    $args = array(
+      'category_name'   =>  'studylog',
+      'posts_per_page'  =>  3
+    );
+    $the_query = new WP_Query($args);
+    if($the_query->have_posts()):
+    ?>
+
     <section id="portfolio">
       <h2>portfolio</h2>
+      <?php while ($the_query->have_posts()): $the_query->the_post();?>
       <div class="works-box">
-        <a href="#">
+        <a href="<?php the_permalink(); ?>">
           <div class="sample"></div>
           <div class="works-box-inner">
             <div class="text-center">
-              <h3>DevLog</h3>
-              <small>2019.07.XX</small>
+              <h3><?php the_title(); ?></h3>
+              <small><?php the_time('Y.m.j'); ?></small>
               <span class="button-readmore uppercase">Read More</span>
             </div>
           </div>
         </a>
+      </div>
+      <?php endwhile; ?>
+      <div class="button">
+        <a href="<?php echo home_url(); ?>/category/portfolio/"><span class="button-readmore uppercase">Read More</span></a>
       </div>
     </section>
     <section id="blog">
