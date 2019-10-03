c13fdd36585f4e007de89232cfec644040328612
Soh1121 <satou.shg@gmail.com>
2019-10-04 00:20:16 +0900

    [update]記事の本文部分のレイアウトを変更
    見やすくするため #70

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,13 @@
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/toppage.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/profile.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/article.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico&display=swap">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
   <script src="https://kit.fontawesome.com/879fc12ee5.js"></script>
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <header>
     <div class="container">
--- single.php
+++ single.php
@@ -1,15 +1,24 @@
 <?php get_header(); ?>
-  <main class="container center">
-    <?php if(have_posts()): ?>
-      <?php while(have_posts()): the_post(); ?>
-        <section <?php post_class(); ?>>
-          <h1><?php the_title(); ?></h1>
-          <?php the_content(); ?>
-        </section>
-      <?php endwhile; ?>
-      <?php comments_template('format=html5'); ?>
-    <?php else: ?>
-      <h2>投稿が見つかりません。</h2>
-    <?php endif; ?>
-  </main>
+<main class="container center">
+  <article id="entry">
+    <div id="main-article">
+      <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+          <section <?php post_class(); ?>>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <div class="postinfo">
+              <i class="far fa-clock"></i>
+              <time class="date updated"><?php the_time('Y年n月j日'); ?></time>
+            </div>
+            <?php the_content(); ?>
+          </section>
+        <?php endwhile; ?>
+    </div>
+    <?php comments_template('format=html5'); ?>
+  <?php else : ?>
+    <h2>投稿が見つかりません。</h2>
+    </div>
+  <?php endif; ?>
+  </article>
+</main>
 <?php get_footer();
