e168a9d8d36b7aabe33d53919c441f3d79583d8a
Soh1121 <satou.shg@gmail.com>
2019-10-15 00:12:10 +0900

    [update]404ページ完成
    無効なURLにも対応できるように #71

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<main class="container center">
-  <section id="404">
-    <div class="white" id="page404">
+<main class="container center" id="page404">
+  <section>
+    <div class="white">
       <h2>ページが見つかりません</h2>
       <p>
         お探しのページが見つかりませんでした。<br>
@@ -11,58 +11,94 @@
   </section>
   <section id="portfolio">
     <h2>portfolio</h2>
-    <div class="works-box">
-      <a href="#">
-        <div class="sample"></div>
-        <div class="works-box-inner">
-          <div class="text-center">
-            <h3>DevLog</h3>
-            <small>2019.07.XX</small>
-            <span class="button-readmore uppercase">Read More</span>
-          </div>
+    <?php
+    // --- 新着ポートフォリオを3件表示 ---
+    $args = array(
+      'category_name'   =>  'portfolio',
+      'posts_per_page'  =>  3
+    );
+    $the_query = new WP_Query($args);
+    if ($the_query->have_posts()) :
+      ?>
+      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+        <div class="works-box">
+          <a href="<?php the_permalink(); ?>">
+            <div class="sample"></div>
+            <div class="works-box-inner">
+              <div class="text-center">
+                <h3><?php the_title(); ?></h3>
+                <small><?php the_time('Y.m.j'); ?></small>
+                <span class="button-readmore uppercase">Read More</span>
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-    </div>
+      <?php endwhile; ?>
+      <div class="button">
+        <a href="<?php echo home_url(); ?>/category/portfolio/"><span class="button-readmore uppercase">Read More</span></a>
+
+      <?php else : ?>
+        <h3>Comming Soon</h3>
+      <?php
+        // -------- WP_query終了-----------
+        wp_reset_postdata();
+      endif;
+      ?>
+      </div>
   </section>
+
   <section id="profile">
     <div class="white">
       <h2>Profile</h2>
-      <a href="profile.html">
-        <img src="../images/Soh-icon.png" alt="Sohの似顔絵イラスト">
+      <a href="<?php echo home_url(); ?>/profile/">
+        <div class="profile-pic">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/Soh-icon.png" alt="Sohの似顔絵イラスト">
+        </div>
         <p>
           ↑ Access ↑
         </p>
       </a>
     </div>
   </section>
-  <section id="blog">
-    <h2>blog</h2>
-    <div class="blog-container">
-      <div class="blog-box">
-        <a href="#">
-          <div class="sample"></div>
-          <h3>タイトル</h3>
-          <p class="right"><small>2019.07.XX</small></p>
-        </a>
+
+  <?php
+  // --- 新着ブログを3件表示 ---
+  $args = array(
+    'category_name'   =>  'studylog', // ポートフォリオ以外のカテゴリを記述
+    'posts_per_page'  =>  3
+  );
+  $the_query = new WP_Query($args);
+  if ($the_query->have_posts()) :
+    ?>
+
+    <section id="blog">
+      <h2>blog</h2>
+      <div class="blog-container">
+        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+          <div class="blog-box">
+            <a href="<?php the_permalink(); ?>">
+              <div class="eyecatch">
+                <?php if (has_post_thumbnail()) : ?>
+                  <?php the_post_thumbnail('thumbnail'); ?>
+                <?php else : ?>
+                  <img src="<?php bloginfo('template_url'); ?>/images/noimage.png" alt="アイキャッチなし" />
+                <?php endif; ?>
+              </div>
+              <h3><?php the_title(); ?></h3>
+              <p class="right"><small><?php the_time('Y.m.j'); ?></small></p>
+            </a>
+          </div>
+        <?php endwhile; ?>
       </div>
-      <div class="blog-box">
-        <a href="#">
-          <div class="sample"></div>
-          <h3>タイトル</h3>
-          <p class="right"><small>2019.07.XX</small></p>
-        </a>
+      <div class="button">
+        <a href="<?php echo home_url(); ?>/list/"><span class=" button-readmore uppercase">Read More</span></a>
       </div>
-      <div class="blog-box">
-        <a href="#">
-          <div class="sample"></div>
-          <h3>タイトル</h3>
-          <p class="right"><small>2019.07.XX</small></p>
-        </a>
-      </div>
-    </div>
-    <div class="button">
-      <a href="#"><span class="button-readmore uppercase">Read More</span></a>
-    </div>
-  </section>
+    </section>
+
+  <?php
+    // -------- 新着情報WP_query終了-----------
+    wp_reset_postdata();
+  endif;
+  ?>
 </main>
 <?php get_footer();
